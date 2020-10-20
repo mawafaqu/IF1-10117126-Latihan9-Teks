4272e49ef7e03ac72e8be9f278a421624f9c40a7
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-10-21 00:46:19 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan9-Teks/src/if1/pkg10117126/latihan9/teks/IF110117126Latihan9Teks.java
@@ -0,0 +1,26 @@
+/*
+ * 
+ * @author
+ * NAMA : Mawa Faqu Rochman
+ * KELAS : PBO1
+ * NIM : 10117126
+ *
+ */
+package if1.pkg10117126.latihan9.teks;
+
+
+public class IF110117126Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
